169e0b180bc70c16c88c237de96661f2033248d6
lumsell <73699283+lumsell@users.noreply.github.com>
2022-09-18 20:21:33 +1000

    Initial commit

---
+++ JsonReader/MainForm.Designer.cs
@@ -0,0 +1,121 @@
+﻿
+namespace JsonReader
+{
+    partial class mainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.loadButton = new System.Windows.Forms.Button();
+            this.saveButton = new System.Windows.Forms.Button();
+            this.errorLabel = new System.Windows.Forms.Label();
+            this.titleLabel = new System.Windows.Forms.Label();
+            this.jsonTable = new System.Windows.Forms.TableLayoutPanel();
+            this.SuspendLayout();
+            // 
+            // loadButton
+            // 
+            this.loadButton.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.loadButton.Location = new System.Drawing.Point(13, 13);
+            this.loadButton.Name = "loadButton";
+            this.loadButton.Size = new System.Drawing.Size(75, 23);
+            this.loadButton.TabIndex = 0;
+            this.loadButton.Text = "Load File";
+            this.loadButton.UseVisualStyleBackColor = true;
+            this.loadButton.Click += new System.EventHandler(this.loadButton_Click);
+            // 
+            // saveButton
+            // 
+            this.saveButton.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.saveButton.Location = new System.Drawing.Point(95, 13);
+            this.saveButton.Name = "saveButton";
+            this.saveButton.Size = new System.Drawing.Size(75, 23);
+            this.saveButton.TabIndex = 1;
+            this.saveButton.Text = "Save";
+            this.saveButton.UseVisualStyleBackColor = true;
+            this.saveButton.Click += new System.EventHandler(this.saveButton_Click);
+            // 
+            // errorLabel
+            // 
+            this.errorLabel.AutoSize = true;
+            this.errorLabel.ForeColor = System.Drawing.Color.DarkRed;
+            this.errorLabel.Location = new System.Drawing.Point(12, 42);
+            this.errorLabel.Name = "errorLabel";
+            this.errorLabel.Size = new System.Drawing.Size(29, 13);
+            this.errorLabel.TabIndex = 2;
+            this.errorLabel.Text = "Error";
+            this.errorLabel.UseWaitCursor = true;
+            // 
+            // titleLabel
+            // 
+            this.titleLabel.AutoSize = true;
+            this.titleLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.titleLabel.Location = new System.Drawing.Point(12, 62);
+            this.titleLabel.Name = "titleLabel";
+            this.titleLabel.Size = new System.Drawing.Size(57, 13);
+            this.titleLabel.TabIndex = 3;
+            this.titleLabel.Text = "Filename";
+            // 
+            // jsonTable
+            // 
+            this.jsonTable.AutoScroll = true;
+            this.jsonTable.ColumnCount = 2;
+            this.jsonTable.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 26.60256F));
+            this.jsonTable.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 73.39744F));
+            this.jsonTable.Location = new System.Drawing.Point(13, 78);
+            this.jsonTable.Name = "jsonTable";
+            this.jsonTable.RowCount = 1;
+            this.jsonTable.RowStyles.Add(new System.Windows.Forms.RowStyle());
+            this.jsonTable.Size = new System.Drawing.Size(312, 370);
+            this.jsonTable.TabIndex = 4;
+            // 
+            // mainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(476, 460);
+            this.Controls.Add(this.jsonTable);
+            this.Controls.Add(this.titleLabel);
+            this.Controls.Add(this.errorLabel);
+            this.Controls.Add(this.saveButton);
+            this.Controls.Add(this.loadButton);
+            this.Name = "mainForm";
+            this.Text = "Viewer";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button loadButton;
+        private System.Windows.Forms.Button saveButton;
+        private System.Windows.Forms.Label errorLabel;
+        private System.Windows.Forms.Label titleLabel;
+        private System.Windows.Forms.TableLayoutPanel jsonTable;
+    }
+}
+
---
+++ JsonReader/MainForm.cs
@@ -0,0 +1,216 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using Newtonsoft.Json;
+
+namespace JsonReader
+{
+    public partial class mainForm : Form
+    {
+        Dictionary<string, object> _attributes;
+        string _filePath = "";
+        public mainForm()
+        {
+            InitializeComponent();
+            titleLabel.Text = "";
+            errorLabel.Text = "";
+        }
+
+        private void loadButton_Click(object sender, EventArgs e)
+        {
+
+            //Show "load file" prompt
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            openFileDialog.Title = "Load File";
+            openFileDialog.Filter = "Json files|*.json";
+            if (openFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                _filePath = openFileDialog.FileName;
+
+                //Display filename
+                titleLabel.Text = Path.GetFileName(_filePath);
+
+                //Clear error text
+                errorLabel.Text = "";
+
+                //Clear all controls from the table in case something was already loaded
+                jsonTable.Controls.Clear();
+
+                try
+                {
+                    var settings = new JsonSerializerSettings
+                    {
+                        //Deserialise as decimal to avoid potential issues with base-2 floating point imprecision 
+                        FloatParseHandling = FloatParseHandling.Decimal
+                    };
+                    _attributes = JsonConvert.DeserializeObject<Dictionary<string, object>>(File.ReadAllText(_filePath), settings);
+                }
+                catch (Exception ex)
+                {
+                    errorLabel.Text += "Error: " + ex.Message + "\n";
+                    return;
+                }
+
+                int i = 0;
+                foreach (KeyValuePair<string, object> attr in _attributes)
+                {
+                    object val = attr.Value;
+                    string key = attr.Key;
+
+                    //Create the key label to be displayed in the left column
+                    Label attrLabel = new Label();
+                    attrLabel.Text = key + ":";
+                    attrLabel.Padding = new Padding(5);
+                    jsonTable.Controls.Add(attrLabel, 0, i);
+
+                    //Create the value field to be displayed in the right column
+                    if (val == null)
+                    {
+                        Label attrNullLabel = new Label();
+                        attrNullLabel.Text = "null";
+                        jsonTable.Controls.Add(attrNullLabel, 1, i);
+                        i++;
+                        continue;
+                    }
+                    Type type = val.GetType();
+                    if (type == typeof(bool))
+                    {
+                        CheckBox attrCheckbox = new CheckBox();
+                        attrCheckbox.Checked = (bool)val;
+                        attrCheckbox.Name = key;
+                        attrCheckbox.CheckedChanged += new System.EventHandler(attrCheckBox_CheckedChanged);
+                        jsonTable.Controls.Add(attrCheckbox, 1, i);
+                    }
+                    else if (type == typeof(string) || type == typeof(long) || type == typeof(decimal))
+                    {
+                        TextBox attrTextBox = new TextBox();
+                        attrTextBox.Text = val.ToString();
+                        attrTextBox.Name = key;
+                        jsonTable.Controls.Add(attrTextBox, 1, i);
+                        //Use a different text change event depending on type, so that the json type is preserved when the file is saved
+                        //And prevent invalid keypresses for integer and decimal values
+                        if (type == typeof(string))
+                        {
+                            attrTextBox.TextChanged += new System.EventHandler(attrTextBox_TextChanged);
+                        }
+                        if (type == typeof(long))
+                        {
+                            attrTextBox.TextChanged += new System.EventHandler(attrTextBoxInt_TextChanged);
+                            attrTextBox.KeyPress += new KeyPressEventHandler(attrTextBoxInt_KeyPress);
+                        }
+                        if (type == typeof(decimal))
+                        {
+                            attrTextBox.TextChanged += new System.EventHandler(attrTextBoxDecimal_TextChanged);
+                            attrTextBox.KeyPress += new KeyPressEventHandler(attrTextBoxDecimal_KeyPress);
+                        }
+                    }
+                    //If the attribute type is complex, use a non-modifiable multiline text box
+                    else if (type == typeof(Newtonsoft.Json.Linq.JObject) || type == typeof(Newtonsoft.Json.Linq.JArray))
+                    {
+                        string labeltext = JsonConvert.SerializeObject(val, Formatting.Indented);
+                        TextBox attrTextBox = new TextBox();
+                        attrTextBox.Text = labeltext;
+                        //Height is fixed so that large objects don't dominate the UI
+                        attrTextBox.Height = 60; 
+                        attrTextBox.Multiline = true;
+                        attrTextBox.ScrollBars = ScrollBars.Both;
+                        attrTextBox.KeyPress += new KeyPressEventHandler(attrTextBoxReadOnly_KeyPress);
+                        jsonTable.Controls.Add(attrTextBox, 1, i);
+                    }
+                    //The following code should never be reached
+                    //If it is, there is a case the system is not handling correctly
+                    else
+                    {
+                        errorLabel.Text += "Error: json file contained object of unexpected type " + type.Name + "\n";
+                    }
+
+                    i++;
+                }
+            }
+
+        }
+
+        private void saveButton_Click(object sender, EventArgs e)
+        {
+            //Show "save file" prompt
+            SaveFileDialog saveFileDialog = new SaveFileDialog();
+            saveFileDialog.Title = "Save As...";
+            saveFileDialog.Filter = "Json files|*.json";
+            saveFileDialog.FileName = _filePath;
+            if (saveFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                _filePath = saveFileDialog.FileName;
+                titleLabel.Text = Path.GetFileName(_filePath);
+                if (_attributes == null)
+                {
+                    //Case where no file was loaded
+                    File.WriteAllText(_filePath, "{\n}");
+                } 
+                else
+                {
+                    File.WriteAllText(_filePath, JsonConvert.SerializeObject(_attributes, Formatting.Indented));
+                }
+            }
+        }
+
+        private void attrTextBoxInt_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            //Prevent the keypress if the input is not a digit or control character
+            if (!char.IsControl(e.KeyChar)
+                && !char.IsDigit(e.KeyChar))
+            {
+                e.Handled = true;
+            }
+        }
+
+        private void attrTextBoxDecimal_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            //Prevent the keypress if the input is not a digit or control character, or if it is a decimal point and there is already one present
+            if (!char.IsControl(e.KeyChar)
+                && !char.IsDigit(e.KeyChar)
+                && (e.KeyChar != '.' || ((TextBox)sender).Text.Contains(".")) 
+                )
+            {
+                e.Handled = true;
+            }
+        }
+
+        private void attrTextBoxReadOnly_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            e.Handled = true;
+        }
+
+
+        //When any modifiable field is changed, the underlying attribute dictionary is updated to match
+        private void attrCheckBox_CheckedChanged(object sender, EventArgs e)
+        {
+            CheckBox attrCheckBox = (CheckBox)sender;
+            _attributes[attrCheckBox.Name] = attrCheckBox.Checked;
+        }
+
+        private void attrTextBox_TextChanged(object sender, EventArgs e)
+        {
+            TextBox attrTextBox = (TextBox)sender;
+            _attributes[attrTextBox.Name] = attrTextBox.Text;
+        }
+
+        private void attrTextBoxInt_TextChanged(object sender, EventArgs e)
+        {
+            TextBox attrTextBox = (TextBox)sender;
+            _attributes[attrTextBox.Name] = Convert.ToInt64(attrTextBox.Text);
+        }
+
+        private void attrTextBoxDecimal_TextChanged(object sender, EventArgs e)
+        {
+            TextBox attrTextBox = (TextBox)sender;
+            _attributes[attrTextBox.Name] = Convert.ToDecimal(attrTextBox.Text);
+        }
+    }
+}
